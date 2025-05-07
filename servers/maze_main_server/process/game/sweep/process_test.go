f7a320158ee475896d3dec7018fde0588f872753
chanyanbing <chanyanbing@17paipai.cn>
2025-05-07 18:03:00 +0800

    合并扫荡服务

---
+++ servers/maze_main_server/process/game/sweep/process_test.go
@@ -0,0 +1,41 @@
+// @Author pangchenyang 2025/3/21 19:28:00
+// @Desc:
+package sweep
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"gitlab.ifreetalk.com/plate/extra/protobuf/proto"
+	"gitlab.ifreetalk.com/plate/freetk/fkcore/fklog"
+	"gitlab.ifreetalk.com/plate/freetk/fkcore/fknet"
+	"gitlab.ifreetalk.com/plate/freetk/fktestutil/testio"
+	_ "gitlab.ifreetalk.com/plate/freetk/fktestutil/testlogger"
+	"gitlab.ifreetalk.com/plate/protodef/MazeGame"
+)
+
+var (
+	logger = fklog.AppLogger().Clone("process_t")
+	tcpCtx = fknet.TCPContext{Context: context.Background(), FkTransportBase: nil, FKLogI: logger}
+)
+
+func TestMain(m *testing.M) {
+	fmt.Println("begin")
+	_ = testio.IOLoad(9)
+	m.Run()
+	fmt.Println("end")
+}
+
+func TestOnStartMazeSweepRQ(t *testing.T) {
+	logger.SetLogId(time.Now().UnixNano())
+	defer func() {
+		time.Sleep(time.Second * 2)
+	}()
+	startSweepRq := &MazeGame.StartMazeSweepRQ{
+		BarrierId: proto.Int32(1),
+	}
+	startSweepRs := &MazeGame.StartMazeSweepRS{}
+	_ = OnStartMazeSweepRQ(tcpCtx, 1, startSweepRq, startSweepRs)
+}
--- servers/maze_main_server/process/process.go
+++ servers/maze_main_server/process/process.go
@@ -1,15 +1,16 @@
 package process
 
 import (
+	"gitlab.ifreetalk.com/maze/maze_game_server/servers/maze_main_server/process/attr_calc"
 	"gitlab.ifreetalk.com/maze/maze_game_server/servers/maze_main_server/process/collect"
 	"gitlab.ifreetalk.com/maze/maze_game_server/servers/maze_main_server/process/equip"
 	"gitlab.ifreetalk.com/maze/maze_game_server/servers/maze_main_server/process/game"
 	"gitlab.ifreetalk.com/maze/maze_game_server/servers/maze_main_server/process/game/energy"
+	"gitlab.ifreetalk.com/maze/maze_game_server/servers/maze_main_server/process/game/sweep"
 	"gitlab.ifreetalk.com/plate/freetk/fkcore/fklog"
 	"gitlab.ifreetalk.com/plate/freetk/fkserver/tcp_service"
-	"gitlab.ifreetalk.com/plate/freetk/fkserver/web_service"
 	"gitlab.ifreetalk.com/plate/freetk/fkserver/thrift_service"
-	"gitlab.ifreetalk.com/maze/maze_game_server/servers/maze_main_server/process/attr_calc"
+	"gitlab.ifreetalk.com/plate/freetk/fkserver/web_service"
 )
 
 func RegisterHandler() {
@@ -17,6 +18,8 @@ func RegisterHandler() {
 	tcp_service.PlugTcpService(func() {
 		// 主功能接口
 		game.RegTcpHandler()
+		// 扫荡功能接口
+		sweep.RegTcpHandler()
 		// 体力相关接口
 		energy.RegTcpHandler()
 		// 装备功能接口
