038af0c923f42f85c483ec4f67e2aa5321990bc6
jiangema <majiange@126.com>
2025-05-09 11:58:28 +0800

    feat(auth): 添加用户登录认证处理逻辑
    添加了用户登录认证的相关处理逻辑，包括注册处理函数、发送数据包接口以及登录请求处理函数。这些改动使得系统能够处理用户登录请求并进行认证，确保用户ID的正确设置。

---
+++ servers/maze_main_server/process/auth/process_login.go
@@ -0,0 +1,34 @@
+package auth
+
+import (
+	"gitlab.ifreetalk.com/plate/extra/protobuf/proto"
+	"gitlab.ifreetalk.com/plate/freetk/common/errors"
+	"gitlab.ifreetalk.com/plate/freetk/fkcore/fknet"
+	"gitlab.ifreetalk.com/plate/freetk/fkcore/fkprometheus"
+	"gitlab.ifreetalk.com/plate/protodef/SysPackDef"
+
+	"go.uber.org/zap"
+)
+
+func OnLoginRQ(ctx fknet.TCPContext, shardingID uint64, rqMsg proto.Message, rsMsg proto.Message) (err error) {
+	fkprometheus.InfoPMT("OnLoginRQ")()
+
+	req := rqMsg.(*SysPackDef.UserLoginRq)
+	res := rsMsg.(*SysPackDef.UserLoginRs)
+
+	logger := ctx
+
+	defer func() {
+		logger.InfoWF("OnLoginRQ end", zap.Any("req", req), zap.Any("res", res))
+	}()
+
+	// 认证
+	// 失败直接返回
+
+	res.Error = errors.NO_ERROR
+
+	// 认证成功设置用户ID, 底层会处理
+	userId := req.GetUserID()
+	ctx.SetTag("userID", userId)
+	return nil
+}
--- servers/maze_main_server/process/process.go
+++ servers/maze_main_server/process/process.go
@@ -3,6 +3,7 @@ package process
 import (
 	"gitlab.ifreetalk.com/maze/maze_game_server/lib/net/websocket_service"
 	"gitlab.ifreetalk.com/maze/maze_game_server/servers/maze_main_server/process/attr_calc"
+	"gitlab.ifreetalk.com/maze/maze_game_server/servers/maze_main_server/process/auth"
 	"gitlab.ifreetalk.com/maze/maze_game_server/servers/maze_main_server/process/buff"
 	"gitlab.ifreetalk.com/maze/maze_game_server/servers/maze_main_server/process/card"
 	"gitlab.ifreetalk.com/maze/maze_game_server/servers/maze_main_server/process/collect"
@@ -22,6 +23,7 @@ import (
 func RegisterHandler() {
 	// 注册Tcp接口
 	websocket_service.PlugTcpRawService(func() {
+		auth.RegisterHandler()
 		// 主功能接口
 		game.RegTcpHandler()
 		// 扫荡功能接口
