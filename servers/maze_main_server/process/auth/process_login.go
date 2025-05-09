05c7b69ff0b141282261e7db6e8d8b5301a6fe8c
jiangema <majiange@126.com>
2025-05-09 15:07:21 +0800

    refactor: 移除不再使用的登录认证和MySQL相关代码
    移除`process_login.go`中不再使用的登录认证逻辑，并删除`UnionIDBindMysql`和`PaipaiUnionIDAuthMysql`两个MySQL相关模块的代码。这些代码已不再需要，简化了代码库结构。

--- servers/maze_main_server/process/auth/process_login.go
+++ servers/maze_main_server/process/auth/process_login.go
@@ -1,7 +1,6 @@
 package auth
 
 import (
-	"gitlab.ifreetalk.com/maze/maze_game_server/usecase/loginauth"
 	"gitlab.ifreetalk.com/plate/extra/protobuf/proto"
 	"gitlab.ifreetalk.com/plate/freetk/common/errors"
 	"gitlab.ifreetalk.com/plate/freetk/fkcore/fknet"
@@ -28,23 +27,6 @@ func OnLoginRQ(ctx fknet.TCPContext, shardingID uint64, rqMsg proto.Message, rsM
 	if req.GetUserID() == 0 {
 		res.Error = errors.COMMON_ERROR_TIPS.Wrap("id is 0")
 		return nil
-	}
-
-	if req.GetUserAuthToken() == 0 {
-		res.Error = errors.COMMON_ERROR_TIPS.Wrap("auth token is 0")
-		return nil
-	}
-
-	loginOK, err := loginauth.AuthJudge(logger, req.GetUserID(), req.GetUserAuthToken())
-	if err != nil {
-		logger.ErrorWF("OnLoginRQ AuthJudge fail", zap.Error(err))
-		res.Error = errors.COMMON_ERROR_TIPS.Wrap("AuthJudge fail")
-		return
-	}
-
-	if !loginOK {
-		res.Error = errors.COMMON_ERROR_TIPS.Wrap("auth token is 0")
-		return
 	}
 
 	res.Error = errors.NO_ERROR
