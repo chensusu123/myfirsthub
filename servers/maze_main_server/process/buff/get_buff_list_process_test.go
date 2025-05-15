5d4697062cba391570ea0ca83ade9a974575de9a
jiangema <majiange@126.com>
2025-05-15 15:08:54 +0800

    test(buff): 更新测试文件中的注释和函数调用
    将注释格式标准化，并在testio.IOLoad函数调用中添加新的参数"90038"，以匹配最新的测试需求

--- servers/maze_main_server/process/buff/get_buff_list_process_test.go
+++ servers/maze_main_server/process/buff/get_buff_list_process_test.go
@@ -26,11 +26,11 @@ var gTestLogger fklog.FKLogI
 
 func TestMain(m *testing.M) {
 	fmt.Println("begin")
-	//initLog()
+	// initLog()
 	rand.Seed(time.Now().UnixNano())
 	gTestLogger = fklog.AppLogger().Clone("test_maze_temp_buff")
 
-	testio.IOLoad(12) //加载N组的io配置
+	testio.IOLoad(12, "90038") // 加载N组的io配置
 	frontcache_service.PlugFCService()
 	fkconfig.EnvVal.AppName = "maze-temp-buff-server"
 	fkconfig.EnvVal.GroupID = 12
