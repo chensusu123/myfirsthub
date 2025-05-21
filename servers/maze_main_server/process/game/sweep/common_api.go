a61c1b70331dc68810903305507069230643e3c9
chanyanbing <chanyanbing@17paipai.cn>
2025-05-21 20:08:00 +0800

    注释无用代码

--- servers/maze_main_server/process/game/sweep/common_api.go
+++ servers/maze_main_server/process/game/sweep/common_api.go
@@ -2,18 +2,18 @@
 // @Desc:
 package sweep
 
-import (
-	"gitlab.ifreetalk.com/plate/extra/protobuf/proto"
-	"gitlab.ifreetalk.com/plate/protodef/Common"
-)
+// import (
+// 	"gitlab.ifreetalk.com/plate/extra/protobuf/proto"
+// 	"gitlab.ifreetalk.com/plate/protodef/Common"
+// )
 
-func ItemsMapToList(itemsMap map[int32]int64) []*Common.Item {
-	itemList := make([]*Common.Item, 0)
-	for itemId, count := range itemsMap {
-		itemList = append(itemList, &Common.Item{
-			ItemId: proto.Int32(itemId),
-			Count:  proto.Int64(count),
-		})
-	}
-	return itemList
-}
+// func ItemsMapToList(itemsMap map[int32]int64) []*Common.Item {
+// 	itemList := make([]*Common.Item, 0)
+// 	for itemId, count := range itemsMap {
+// 		itemList = append(itemList, &Common.Item{
+// 			ItemId: proto.Int32(itemId),
+// 			Count:  proto.Int64(count),
+// 		})
+// 	}
+// 	return itemList
+// }
