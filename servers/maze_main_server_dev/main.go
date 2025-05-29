fff31f5d10b86c56b48f1fa73787e21edcb05753
jiangema <majiange@126.com>
2025-05-29 16:25:35 +0800

    refactor(main): 根据环境变量调整业务逻辑初始化
    在本地开发环境时使用自定义业务加载配置，否则初始化配置RPC客户端
    ```
    
    这个提交消息：
    1. 使用了`refactor`类型，因为这是对代码结构的调整而非功能修改
    2. 添加了`(main)`范围，因为修改的是主函数
    3. 描述部分简明扼要地说明了修改内容（"根据环境变量调整业务逻辑初始化"）
    4. 在正文中补充说明了不同环境下的具体行为差异
    5. 遵循了中文提交信息的规范，使用了简洁明了的表达方式

--- servers/maze_main_server_dev/main.go
+++ servers/maze_main_server_dev/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"gitlab.ifreetalk.com/maze-plate/freetk/fkserver"
 	"maze_game_server/servers/maze_main_server/process"
 	"maze_game_server/usecase/business"
 	"maze_game_server/usecase/tasktimer"
+
+	"gitlab.ifreetalk.com/maze-plate/freetk/fkcore/fkconfig"
+	"gitlab.ifreetalk.com/maze-plate/freetk/fkserver"
+	"gitlab.ifreetalk.com/maze-plate/freetk/fkserver/config_manager/loadconfigapi"
 )
 
 // 19987	UN_CGK_SVR_TYPE_MAZE_MAIN_SERVER 小程序版迷宫主服务
@@ -13,7 +16,13 @@ func main() {
 
 	process.RegisterHandler()
 
-	fkserver.AddBusiness(&business.GCustomBusiness)
+	if fkconfig.EnvVal.IsLocalDev {
+		fkserver.AddBusiness(&business.GCustomBusiness)
+		loadconfigapi.SetLoadConfigFunc(business.GCustomBusiness.LoadCacheConfig)
+		loadconfigapi.SetInitConfigCacheFunc(business.GCustomBusiness.Init)
+	} else {
+		loadconfigapi.InitConfigRpcClient()
+	}
 	fkserver.AddBusiness(tasktimer.GTaskTimerBusiness)
 
 	fkserver.Run()
