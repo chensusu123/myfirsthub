1b94cecb67f70cae01fddef3eaa3b60b6ae4d355
jiangema <majiange@126.com>
2025-06-03 11:43:33 +0800

    feat(auth): 添加配置数据MD5校验功能
    新增处理配置数据MD5校验请求的功能，包括注册处理器和实现具体逻辑。该功能用于客户端校验配置数据版本一致性，通过返回配置表的文件名、MD5和表名信息。

--- servers/maze_main_server/process/auth/process_main.go
+++ servers/maze_main_server/process/auth/process_main.go
@@ -14,4 +14,8 @@ func RegisterHandler() {
 		10494, &UserLogin.UserLiveRq{},
 		10495, &UserLogin.UserLiveRs{},
 		OnLiveRQ)
+	websocket_service.RegProcSimple(
+		10500, &UserLogin.ConfigDataMd5Rq{},
+		10501, &UserLogin.ConfigDataMd5Rs{},
+		OnConfigDataMd5Rq)
 }
