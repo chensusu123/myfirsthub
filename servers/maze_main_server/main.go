96fb5e00a4f3b93df9239171030c95ee7add7292
jiangema <majiange@126.com>
2025-06-03 17:01:50 +0800

    feat(maze_main_server): 添加polaris服务配置和初始化逻辑
    添加polaris.yaml配置文件用于服务发现和配置管理
    在非本地开发环境下初始化polaris消息服务并注册到业务中
    移除被注释的旧配置代码，优化服务启动流程

--- servers/maze_main_server/main.go
+++ servers/maze_main_server/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"maze_game_server/lib/net/polarismessvc"
 	"maze_game_server/servers/maze_main_server/process"
 	"maze_game_server/usecase/business"
+	"maze_game_server/usecase/naming"
 	"maze_game_server/usecase/tasktimer"
 
+	"gitlab.ifreetalk.com/maze-plate/freetk/fkcore/fkconfig"
 	"gitlab.ifreetalk.com/maze-plate/freetk/fkserver"
 	"gitlab.ifreetalk.com/maze-plate/freetk/fkserver/config_manager/loadconfigapi"
+	"gitlab.ifreetalk.com/maze-plate/freetk/mockio"
 )
 
 // 19987	UN_CGK_SVR_TYPE_MAZE_MAIN_SERVER 小程序版迷宫主服务
@@ -15,16 +19,19 @@ func main() {
 	// process.RegisterHandler()
 
 	process.RegisterHandler()
-	// if fkconfig.EnvVal.IsLocalDev {
-	// 	fkserver.AddBusiness(&business.GCustomBusiness)
-	// 	loadconfigapi.SetLoadConfigFunc(business.GCustomBusiness.LoadCacheConfig)
-	// 	loadconfigapi.SetInitConfigCacheFunc(business.GCustomBusiness.Init)
-	// } else {
-	// 	loadconfigapi.InitConfigRpcClient()
-	// }
-	fkserver.AddBusiness(&business.GCustomBusiness)
-	loadconfigapi.SetLoadConfigFunc(business.GCustomBusiness.LoadCacheConfig)
-	loadconfigapi.SetInitConfigCacheFunc(business.GCustomBusiness.Init)
+	if fkconfig.EnvVal.IsLocalDev {
+		fkserver.AddBusiness(&business.GCustomBusiness)
+		loadconfigapi.SetLoadConfigFunc(business.GCustomBusiness.LoadCacheConfig)
+		loadconfigapi.SetInitConfigCacheFunc(business.GCustomBusiness.Init)
+	} else {
+		mockio.SetNaming(naming.NewClientSuite("./conf.d/polaris.yaml"))
+		// loadconfigapi.InitConfigRpcClient()
+		fkserver.AddBusiness(&business.GCustomBusiness)
+		loadconfigapi.SetLoadConfigFunc(business.GCustomBusiness.LoadCacheConfig)
+		loadconfigapi.SetInitConfigCacheFunc(business.GCustomBusiness.Init)
+		polarismessSvc := polarismessvc.NewPolarismesSvc()
+		fkserver.AddBusiness(polarismessSvc)
+	}
 	// fkserver.AddBusiness(&business.GCustomBusiness)
 	fkserver.AddBusiness(tasktimer.GTaskTimerBusiness)
 	fkserver.Run()
