045a30e310e74561cb8353b102fc98e7663587d2
hujiahong <hujiahong@17paipai.cn>
2025-06-05 22:00:37 +0800

    jenkins自动提交-20250605-22_00_35

--- config/GMazeAnswerMechanismV8Cfg/MazeAnswerMechanismV8.go
+++ config/GMazeAnswerMechanismV8Cfg/MazeAnswerMechanismV8.go
@@ -1,28 +1,26 @@
 package GMazeAnswerMechanismV8Cfg
 
-
 import (
+	"errors"
+	"gitlab.ifreetalk.com/maze-plate/freetk/fkcore/fklog"
+	"gitlab.ifreetalk.com/maze-plate/freetk/fkserver/config_manager"
+	"go.uber.org/zap"
+	"strconv"
 	"sync"
 	"sync/atomic"
 	"unsafe"
-	"strconv"
-	"errors"
-	"gitlab.ifreetalk.com/maze-plate/freetk/fkserver/config_manager"
-	"gitlab.ifreetalk.com/maze-plate/freetk/fkcore/fklog"
-	"go.uber.org/zap"
 )
-
 
 // MazeAnswerMechanismV8ConfigRow from maze_answer_mechanism_v8【答题机关词库】.xlsx maze_answer_mechanism_v8
 type MazeAnswerMechanismV8ConfigRow struct {
-    Id       int32  `json:"id"` // 唯一id
-    Question bank_id       int32  `json:"question bank_id"` // 题库id
-    Question_stem       string  `json:"question_stem"` // 题干
-    Answer_a       string  `json:"answer_a"` // 选项a
-    Answer_b       string  `json:"answer_b"` // 选项b
-    Answer_c       string  `json:"answer_c"` // 选项c
-    Answer_d       string  `json:"answer_d"` // 选项d
-    Right_answer       int32  `json:"right_answer"` // 正确选项
+	Id               int32  `json:"id"`               // 唯一id
+	Question_bank_id int32  `json:"question_bank_id"` // 题库id
+	Question_stem    string `json:"question_stem"`    // 题干
+	Answer_a         string `json:"answer_a"`         // 选项a
+	Answer_b         string `json:"answer_b"`         // 选项b
+	Answer_c         string `json:"answer_c"`         // 选项c
+	Answer_d         string `json:"answer_d"`         // 选项d
+	Right_answer     int32  `json:"right_answer"`     // 正确选项
 }
 
 // MazeAnswerMechanismV8Config from maze_answer_mechanism_v8【答题机关词库】.xlsx maze_answer_mechanism_v8
@@ -58,7 +56,7 @@ func (c *MazeAnswerMechanismV8Config) Get(configId int32) *MazeAnswerMechanismV8
 }
 
 // GetAllMazeAnswerMechanismV8Config get all config slice
-func (c *MazeAnswerMechanismV8Config)  GetAllMazeAnswerMechanismV8Config () (res []*MazeAnswerMechanismV8ConfigRow) {
+func (c *MazeAnswerMechanismV8Config) GetAllMazeAnswerMechanismV8Config() (res []*MazeAnswerMechanismV8ConfigRow) {
 	// c.lock.RLock()
 	// defer c.lock.RUnlock()
 	for _, val := range c.ConfigRows {
@@ -68,7 +66,7 @@ func (c *MazeAnswerMechanismV8Config)  GetAllMazeAnswerMechanismV8Config () (res
 }
 
 // GetAll get all config slice
-func (c *MazeAnswerMechanismV8Config)  GetAll() (res []*MazeAnswerMechanismV8ConfigRow) {
+func (c *MazeAnswerMechanismV8Config) GetAll() (res []*MazeAnswerMechanismV8ConfigRow) {
 	// c.lock.RLock()
 	// defer c.lock.RUnlock()
 	for _, val := range c.ConfigRows {
@@ -77,9 +75,8 @@ func (c *MazeAnswerMechanismV8Config)  GetAll() (res []*MazeAnswerMechanismV8Con
 	return
 }
 
-
-// global config pointer 
-var gConfigData *MazeAnswerMechanismV8Config 
+// global config pointer
+var gConfigData *MazeAnswerMechanismV8Config
 
 // GetMazeAnswerMechanismV8Config pkg func. get one config by configId
 func GetMazeAnswerMechanismV8Config(configId int32) *MazeAnswerMechanismV8ConfigRow {
@@ -92,8 +89,8 @@ func Get(configId int32) *MazeAnswerMechanismV8ConfigRow {
 }
 
 // GetAllMazeAnswerMechanismV8Config pkg func. get all config slice
-func GetAllMazeAnswerMechanismV8Config () []*MazeAnswerMechanismV8ConfigRow {
-	return gConfigData.GetAllMazeAnswerMechanismV8Config ()
+func GetAllMazeAnswerMechanismV8Config() []*MazeAnswerMechanismV8ConfigRow {
+	return gConfigData.GetAllMazeAnswerMechanismV8Config()
 }
 
 // GetAll pkg func. get all config slice
@@ -102,17 +99,17 @@ func GetAll() []*MazeAnswerMechanismV8ConfigRow {
 }
 
 // ConfigRows get raw map data
-func ConfigRows() map[int32]*MazeAnswerMechanismV8ConfigRow{
+func ConfigRows() map[int32]*MazeAnswerMechanismV8ConfigRow {
 	return gConfigData.ConfigRows
 }
 
 // GetConfigDesc get config desc for lod,debug etc.
-func GetConfigDesc() string{
+func GetConfigDesc() string {
 	return "MazeAnswerMechanismV8ConfigRow from maze_answer_mechanism_v8【答题机关词库】.xlsx maze_answer_mechanism_v8"
 }
 
 // GetRawValue get raw data
-func GetRawValue() *MazeAnswerMechanismV8Config{
+func GetRawValue() *MazeAnswerMechanismV8Config {
 	return gConfigData
 }
 
@@ -122,10 +119,10 @@ func SheetName() string {
 }
 
 func init() {
-	// reg config auto load 
-	config_manager.RegAutoConfig("maze_answer_mechanism_v8.json", 
+	// reg config auto load
+	config_manager.RegAutoConfig("maze_answer_mechanism_v8.json",
 		"maze_answer_mechanism_v8【答题机关词库】.xlsx", "maze_answer_mechanism_v8",
-	 	&gMazeAnswerMechanismV8Parser{}, &gMazeAnswerMechanismV8Loader{})
+		&gMazeAnswerMechanismV8Parser{}, &gMazeAnswerMechanismV8Loader{})
 }
 
 // data update call back
@@ -153,32 +150,35 @@ var cfgCheckCallBack sync.Map //map[string]func(*MazeAnswerMechanismV8Config)err
 // doConfigCheckCallback do config check callback
 func doConfigCheckCallback(c *MazeAnswerMechanismV8Config) (err error) {
 	cfgCheckCallBack.Range(func(key, value interface{}) bool {
-		err := value.(func(*MazeAnswerMechanismV8Config)error)(c)
+		err := value.(func(*MazeAnswerMechanismV8Config) error)(c)
 		return err == nil
 	})
-	return 
+	return
 }
 
 // RegisterLoadedCallBack reg config update func.
-func RegisterConfigCheck(key string, f func(*MazeAnswerMechanismV8Config)error) {
+func RegisterConfigCheck(key string, f func(*MazeAnswerMechanismV8Config) error) {
 	cfgCheckCallBack.Store(key, f)
 }
 
 // implete ConfigLoader interface
 type gMazeAnswerMechanismV8Loader struct {
 }
+
 // NewContainer new data container pointer
-func (*gMazeAnswerMechanismV8Loader) NewContainer() interface{}{
+func (*gMazeAnswerMechanismV8Loader) NewContainer() interface{} {
 	return newConfig()
 }
+
 // Check check new config data ptr
-func (*gMazeAnswerMechanismV8Loader) Check(newPtr interface{})error{
+func (*gMazeAnswerMechanismV8Loader) Check(newPtr interface{}) error {
 	// set global ptr
 	cfgData := newPtr.(*MazeAnswerMechanismV8Config)
 	return doConfigCheckCallback(cfgData)
 }
+
 // Swap swap global config data ptr
-func (*gMazeAnswerMechanismV8Loader) Swap(newPtr interface{}){
+func (*gMazeAnswerMechanismV8Loader) Swap(newPtr interface{}) {
 	// convert pointer
 	cache := newPtr.(*MazeAnswerMechanismV8Config)
 	// update second edit
@@ -186,45 +186,48 @@ func (*gMazeAnswerMechanismV8Loader) Swap(newPtr interface{}){
 	// set global ptr
 	atomic.StorePointer((*unsafe.Pointer)(unsafe.Pointer(&gConfigData)), unsafe.Pointer(cache))
 }
+
 // Add append config item to map,save result
-func (*gMazeAnswerMechanismV8Loader) Add(logger fklog.FKLogI,container interface{}, ri interface{})(err error) {
-	row,ok := ri.(*MazeAnswerMechanismV8ConfigRow)
+func (*gMazeAnswerMechanismV8Loader) Add(logger fklog.FKLogI, container interface{}, ri interface{}) (err error) {
+	row, ok := ri.(*MazeAnswerMechanismV8ConfigRow)
 	if !ok {
 		err = errors.New("invalid type. not *MazeAnswerMechanismV8ConfigRow")
 		logger.ErrorWF("invalid type. not *MazeAnswerMechanismV8ConfigRow", zap.String("xlsx", "maze_answer_mechanism_v8【答题机关词库】.xlsx"),
 			zap.String("sheet", "maze_answer_mechanism_v8"))
-		return 
+		return
 	}
-	config,ok := container.(*MazeAnswerMechanismV8Config)
+	config, ok := container.(*MazeAnswerMechanismV8Config)
 	if !ok {
 		err = errors.New("invalid type. not *MazeAnswerMechanismV8Config")
 		logger.ErrorWF("invalid type. not *MazeAnswerMechanismV8Config", zap.String("xlsx", "maze_answer_mechanism_v8【答题机关词库】.xlsx"),
 			zap.String("sheet", "maze_answer_mechanism_v8"))
-		return 
+		return
 	}
 	config.ConfigRows[row.Id] = row
 	return
 }
+
 // GetValue get real map value for json parse
-func (*gMazeAnswerMechanismV8Loader) GetValue(logger fklog.FKLogI,container interface{})(real interface{},err error) {
-	config,ok := container.(*MazeAnswerMechanismV8Config)
+func (*gMazeAnswerMechanismV8Loader) GetValue(logger fklog.FKLogI, container interface{}) (real interface{}, err error) {
+	config, ok := container.(*MazeAnswerMechanismV8Config)
 	if !ok {
 		err = errors.New("invalid type. not *MazeAnswerMechanismV8Config")
 		logger.ErrorWF("invalid type. not *MazeAnswerMechanismV8Config", zap.String("xlsx", "maze_answer_mechanism_v8【答题机关词库】.xlsx"),
 			zap.String("sheet", "maze_answer_mechanism_v8"))
-		return 
+		return
 	}
 	real = &config.ConfigRows
 	return
 }
+
 // Range range all data for json append data parse.
-func (*gMazeAnswerMechanismV8Loader) Range(logger fklog.FKLogI, container interface{}, rf func(row interface{}) error) (err error){
-	config,ok := container.(*MazeAnswerMechanismV8Config)
+func (*gMazeAnswerMechanismV8Loader) Range(logger fklog.FKLogI, container interface{}, rf func(row interface{}) error) (err error) {
+	config, ok := container.(*MazeAnswerMechanismV8Config)
 	if !ok {
 		err = errors.New("invalid type. not *MazeAnswerMechanismV8Config")
 		logger.ErrorWF("invalid type. not *MazeAnswerMechanismV8Config", zap.String("xlsx", "maze_answer_mechanism_v8【答题机关词库】.xlsx"),
 			zap.String("sheet", "maze_answer_mechanism_v8"))
-		return 
+		return
 	}
 	for _, row := range config.ConfigRows {
 		err = rf(row)
@@ -235,10 +238,10 @@ func (*gMazeAnswerMechanismV8Loader) Range(logger fklog.FKLogI, container interf
 	return
 }
 
-
 // implete ConfigParser interface
 type gMazeAnswerMechanismV8Parser struct {
 }
+
 // New new config row data
 func (*gMazeAnswerMechanismV8Parser) New() interface{} {
 	return &MazeAnswerMechanismV8ConfigRow{}
@@ -248,89 +251,90 @@ func (*gMazeAnswerMechanismV8Parser) New() interface{} {
 func (*gMazeAnswerMechanismV8Parser) Fields() []string {
 	return gMazeAnswerMechanismV8Fields
 }
+
 // Parse parse raw data to row data
-func (*gMazeAnswerMechanismV8Parser) Parse(logger fklog.FKLogI,data []string, row interface{}) (err error) {
+func (*gMazeAnswerMechanismV8Parser) Parse(logger fklog.FKLogI, data []string, row interface{}) (err error) {
 	// convert row type
-	config,ok := row.(*MazeAnswerMechanismV8ConfigRow)
+	config, ok := row.(*MazeAnswerMechanismV8ConfigRow)
 	if !ok {
 		err = errors.New("invalid type. not *MazeAnswerMechanismV8ConfigRow")
 		logger.ErrorWF("invalid type. not *MazeAnswerMechanismV8ConfigRow", zap.String("xlsx", "maze_answer_mechanism_v8【答题机关词库】.xlsx"),
 			zap.String("sheet", "maze_answer_mechanism_v8"))
-		return 
+		return
 	}
 	// compare length
 	if len(data) != len(gMazeAnswerMechanismV8Fields) {
 		err = errors.New("fields count not match.")
-		logger.ErrorWF("invalid type. not *map[int32]*MazeAnswerMechanismV8ConfigRow", 
+		logger.ErrorWF("invalid type. not *map[int32]*MazeAnswerMechanismV8ConfigRow",
 			zap.String("xlsx", "maze_answer_mechanism_v8【答题机关词库】.xlsx"),
-			zap.String("sheet", "maze_answer_mechanism_v8"), zap.Int("need_count",len(gMazeAnswerMechanismV8Fields)), 
-			zap.Int("had_count",len(data)))
+			zap.String("sheet", "maze_answer_mechanism_v8"), zap.Int("need_count", len(gMazeAnswerMechanismV8Fields)),
+			zap.Int("had_count", len(data)))
 		return
 	}
 
 	var tmp int64
 
-	// parse column 0 id : 唯一id 
+	// parse column 0 id : 唯一id
 	if data[0] != "" {
-		tmp,err = strconv.ParseInt(data[0],10,64)
+		tmp, err = strconv.ParseInt(data[0], 10, 64)
 		if err != nil {
 			err = errors.New("parse field id 唯一id to int32 failed")
-			logger.ErrorWF("parse field id 唯一id to int32 failed.", 
-				zap.String("xlsx", "maze_answer_mechanism_v8【答题机关词库】.xlsx"), zap.String("sheet", "maze_answer_mechanism_v8"), 
-				zap.String("parse_data",data[0]), 
+			logger.ErrorWF("parse field id 唯一id to int32 failed.",
+				zap.String("xlsx", "maze_answer_mechanism_v8【答题机关词库】.xlsx"), zap.String("sheet", "maze_answer_mechanism_v8"),
+				zap.String("parse_data", data[0]),
 				zap.Error(err))
 			return
 		}
 		config.Id = int32(tmp)
 	}
 
-	// parse column 1 question bank_id : 题库id 
+	// parse column 1 question_bank_id : 题库id
 	if data[1] != "" {
-		tmp,err = strconv.ParseInt(data[1],10,64)
+		tmp, err = strconv.ParseInt(data[1], 10, 64)
 		if err != nil {
-			err = errors.New("parse field question bank_id 题库id to int32 failed")
-			logger.ErrorWF("parse field question bank_id 题库id to int32 failed.", 
-				zap.String("xlsx", "maze_answer_mechanism_v8【答题机关词库】.xlsx"), zap.String("sheet", "maze_answer_mechanism_v8"), 
-				zap.String("parse_data",data[1]), 
+			err = errors.New("parse field question_bank_id 题库id to int32 failed")
+			logger.ErrorWF("parse field question_bank_id 题库id to int32 failed.",
+				zap.String("xlsx", "maze_answer_mechanism_v8【答题机关词库】.xlsx"), zap.String("sheet", "maze_answer_mechanism_v8"),
+				zap.String("parse_data", data[1]),
 				zap.Error(err))
 			return
 		}
-		config.Question bank_id = int32(tmp)
+		config.Question_bank_id = int32(tmp)
 	}
 
-	// parse column 2 question_stem : 题干 
+	// parse column 2 question_stem : 题干
 	if data[2] != "" {
 		config.Question_stem = data[2]
 	}
 
-	// parse column 3 answer_a : 选项a 
+	// parse column 3 answer_a : 选项a
 	if data[3] != "" {
 		config.Answer_a = data[3]
 	}
 
-	// parse column 4 answer_b : 选项b 
+	// parse column 4 answer_b : 选项b
 	if data[4] != "" {
 		config.Answer_b = data[4]
 	}
 
-	// parse column 5 answer_c : 选项c 
+	// parse column 5 answer_c : 选项c
 	if data[5] != "" {
 		config.Answer_c = data[5]
 	}
 
-	// parse column 6 answer_d : 选项d 
+	// parse column 6 answer_d : 选项d
 	if data[6] != "" {
 		config.Answer_d = data[6]
 	}
 
-	// parse column 7 right_answer : 正确选项 
+	// parse column 7 right_answer : 正确选项
 	if data[7] != "" {
-		tmp,err = strconv.ParseInt(data[7],10,64)
+		tmp, err = strconv.ParseInt(data[7], 10, 64)
 		if err != nil {
 			err = errors.New("parse field right_answer 正确选项 to int32 failed")
-			logger.ErrorWF("parse field right_answer 正确选项 to int32 failed.", 
-				zap.String("xlsx", "maze_answer_mechanism_v8【答题机关词库】.xlsx"), zap.String("sheet", "maze_answer_mechanism_v8"), 
-				zap.String("parse_data",data[7]), 
+			logger.ErrorWF("parse field right_answer 正确选项 to int32 failed.",
+				zap.String("xlsx", "maze_answer_mechanism_v8【答题机关词库】.xlsx"), zap.String("sheet", "maze_answer_mechanism_v8"),
+				zap.String("parse_data", data[7]),
 				zap.Error(err))
 			return
 		}
@@ -340,22 +344,22 @@ func (*gMazeAnswerMechanismV8Parser) Parse(logger fklog.FKLogI,data []string, ro
 }
 
 var gMazeAnswerMechanismV8Fields = []string{
-    "id",
-    "question bank_id",
-    "question_stem",
-    "answer_a",
-    "answer_b",
-    "answer_c",
-    "answer_d",
-    "right_answer",
+	"id",
+	"question_bank_id",
+	"question_stem",
+	"answer_a",
+	"answer_b",
+	"answer_c",
+	"answer_d",
+	"right_answer",
 }
 
 // LoadDataManual load data for test
-func LoadDataManual(logger fklog.FKLogI, load func(file, sheet string, fields []string) ([][]string,error)) (err error) {
+func LoadDataManual(logger fklog.FKLogI, load func(file, sheet string, fields []string) ([][]string, error)) (err error) {
 	parser := &gMazeAnswerMechanismV8Parser{}
 	loader := &gMazeAnswerMechanismV8Loader{}
 	var data [][]string
-	data,err = load("maze_answer_mechanism_v8【答题机关词库】.xlsx", "maze_answer_mechanism_v8", gMazeAnswerMechanismV8Fields)
+	data, err = load("maze_answer_mechanism_v8【答题机关词库】.xlsx", "maze_answer_mechanism_v8", gMazeAnswerMechanismV8Fields)
 	if err != nil {
 		logger.ErrorWF("load maze_answer_mechanism_v8【答题机关词库】.xlsx maze_answer_mechanism_v8 data failed.", zap.Error(err))
 		return
