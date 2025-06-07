be5815f1b37c55ccf220546c666f8917fe8f3c93
jiangema <majiange@126.com>
2025-06-07 10:49:52 +0800

    feat(mysql): 在GetShardingDbName中添加日志记录
    添加日志记录以跟踪分库名称的生成过程，便于调试和问题排查

--- io/mysql/mysql_init.go
+++ io/mysql/mysql_init.go
@@ -15,6 +15,7 @@ import (
 	"gitlab.ifreetalk.com/maze-plate/freetk/fkcore/fklog"
 	"gitlab.ifreetalk.com/maze-plate/freetk/fkcore/fkmysql"
 	"gitlab.ifreetalk.com/maze-plate/freetk/pkg/naming"
+	"go.uber.org/zap"
 )
 
 type mysqlConfig struct {
@@ -111,6 +112,9 @@ func GetShardingTableName(baseTable string) string {
 
 // 获取分库名字
 func GetShardingDbName(baseDb string) string {
+	fklog.AppLogger().Clone("").InfoWF("GetShardingDbName", zap.Any("baseDb", baseDb),
+		zap.Any("EnvVal", fkconfig.EnvVal),
+		zap.Any("mysqlCfg", mysqlCfg))
 	if fkconfig.EnvVal.IsLocalDev == true {
 		return mysqlCfg.DbName
 	}
