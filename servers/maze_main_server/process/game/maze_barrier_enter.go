86cc86ed46cef151f572af8a89c1a7d9ddd50dbf
zsai001 <zsai001@qq.com>
2025-07-19 16:45:32 +0800

    add storage maze new packet.

--- servers/maze_main_server/process/game/maze_barrier_enter.go
+++ servers/maze_main_server/process/game/maze_barrier_enter.go
@@ -81,29 +81,6 @@ func (g *Game) OnMazeBarrierEnterRQ_10447_10448(s *session.Session, req *MazeGam
 	//	res.Energy = proto.Int32(userInfo.Energy)
 	var isNewBarrier bool
 
-	// 默认是从存档进入
-	storageInfo, err := syncmazestorageinforedis.GetSyncMazeStorageInfo(userId, userInfo.Barrier)
-	if err != nil {
-		logger.ErrorWF("OnMazeBarrierEnterRQ GetSyncMazeStorageInfo fail", zap.Error(err))
-		return
-	}
-	res.StorageInfo = storageInfo
-	if storageInfo == nil {
-		// 进入清临时buff
-		mazebarriertempbuffredis.ClearBarrierTempBuff(logger, userId, req.GetBarrierId())
-		mazebuffinforedis.DelMazeBuffBySrc(logger, userId, constdef.MazeBuffSrcSelectBuffForce)
-		// 推送属性计算消息
-		calcAttrNotify := &structsdef.MazeCalcAttrNotifyMsg{
-			UserId:  userId,
-			ChgType: constdef.MazeBuffChgForceValue,
-			Session: "buff",
-			BuffSrc: constdef.MazeBuffSrcSelectBuffForce,
-		}
-		mazeattrcalcnotifyqueue.SendMazeAttrCalcNotify(logger, calcAttrNotify)
-		// 清理关卡操作状态
-		mazebarrieropstatusredis.ClearOpStatus(logger, userId, req.GetBarrierId())
-	}
-
 	mazeBattleInfo, err3 := GetMazeBattleData(logger, userId, req.GetBarrierId())
 	if err3 != nil {
 		logger.ErrorWF("OnMazeBarrierEnterRQ GetMazeBattleData fail", zap.Error(err3))
@@ -289,6 +266,62 @@ func SubUserEnergy(logger fklog.FKLogI, uid uint64, subEnergy int32) (isSucc boo
 		err = errors.New(string(res.GetErrInfo().GetErrMsg()))
 		return false, res.GetRemainVal(), err
 	}
+}
+
+func (g *Game) OnGetStorageInfoRQ_10529_10530(s *session.Session, req *MazeGame.MazeBarrierEnterRQ) (err error) {
+	defer fkprometheus.InfoPMT("OnGetStorageInfoRQ")()
+
+	logger := log.Clone("Game", uint64(s.UID()), 0)
+	res := &MazeGame.GetStorageInfoRS{}
+
+	logger.InfoWF("OnGetStorageInfoRQ start", zap.Any("req", req))
+	defer func() {
+		err = s.Response(res)
+		logger.InfoWF("OnGetStorageInfoRQ end", zap.Any("res", res))
+	}()
+
+	res.Header = req.Header
+	res.ErrInfo = errors.NO_ERROR
+
+	userId := uint64(s.UID())
+
+	if req.GetBarrierId() <= 0 {
+		logger.ErrorWF("OnGetStorageInfoRQ req barrier invalid", zap.Any("req", req))
+		res.ErrInfo = errors.COMMON_ERROR_TIPS.Wrap("关卡id未设置")
+		return
+	}
+
+	userInfo, err := mazeuserinfo.GetUserInfoV2(logger, userId)
+	if err != nil {
+		logger.ErrorWF("OnGetStorageInfoRQ GetUserInfoV2 fail", zap.Error(err))
+		res.ErrInfo = errors.MODULE_ERROR.ToInfo()
+		return
+	}
+
+	// 默认是从存档进入
+	storageInfo, err := syncmazestorageinforedis.GetSyncMazeStorageInfo(userId, userInfo.Barrier)
+	if err != nil {
+		logger.ErrorWF("OnGetStorageInfoRQ GetSyncMazeStorageInfo fail", zap.Error(err))
+		return
+	}
+	res.StorageInfo = storageInfo
+	if storageInfo == nil {
+		// 进入清临时buff
+		mazebarriertempbuffredis.ClearBarrierTempBuff(logger, userId, req.GetBarrierId())
+		mazebuffinforedis.DelMazeBuffBySrc(logger, userId, constdef.MazeBuffSrcSelectBuffForce)
+		// 推送属性计算消息
+		calcAttrNotify := &structsdef.MazeCalcAttrNotifyMsg{
+			UserId:  userId,
+			ChgType: constdef.MazeBuffChgForceValue,
+			Session: "buff",
+			BuffSrc: constdef.MazeBuffSrcSelectBuffForce,
+		}
+		mazeattrcalcnotifyqueue.SendMazeAttrCalcNotify(logger, calcAttrNotify)
+		// 清理关卡操作状态
+		mazebarrieropstatusredis.ClearOpStatus(logger, userId, req.GetBarrierId())
+	}
+
+	return nil
 }
 
 func GetUserMoney(logger fklog.FKLogI, uid uint64) {
