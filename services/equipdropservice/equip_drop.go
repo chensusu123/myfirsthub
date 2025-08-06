e6d7cb0b2fc8fee2bd56129db206b6ec7df88882
renyingliang <renyingliang@17paipai.cn>
2025-08-06 17:54:08 +0800

    杀怪掉落装备改版

--- services/equipdropservice/equip_drop.go
+++ services/equipdropservice/equip_drop.go
@@ -63,7 +63,7 @@ func (s service) getEquipId(logger fklog.FKLogI, userId uint64, mazeLevel int32,
 		return nil, err
 	}
 	equipMap = make(map[int32]int32)
-	if cfg.Special_drop != nil && info.SpecialDropIndex < int32(len(cfg.Special_drop)-2) {
+	if cfg.Special_drop != nil && info.SpecialDropIndex < int32(len(cfg.Special_drop)-1) {
 		dropMap, addNum := s.specialEquipDrop(logger, userId, barrier, mazeLevel, addCount)
 		if len(dropMap) > 0 {
 			for k, v := range dropMap {
@@ -107,7 +107,7 @@ func (s service) specialEquipDrop(logger fklog.FKLogI, userId uint64, barrier, m
 		return nil, 0
 	}
 
-	if info.SpecialDropIndex >= int32(len(cfg.Special_drop)-2) {
+	if info.SpecialDropIndex >= int32(len(cfg.Special_drop)-1) {
 		logger.InfoWF("specialEquipDrop special_drop max limit")
 		return nil, 0
 	}
@@ -128,7 +128,7 @@ func (s service) specialEquipDrop(logger fklog.FKLogI, userId uint64, barrier, m
 		equipId := getEquipId(newLevel, quality, pos)
 		equipIdMap[equipId] += 1
 
-		info.SpecialDropIndex = int32(i)
+		info.SpecialDropIndex = int32(i + 1)
 		add++
 	}
 
