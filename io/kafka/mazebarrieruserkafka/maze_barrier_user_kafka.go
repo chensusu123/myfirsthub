869e422e8184cff1f0fe30894e0e02140047286c
caojun <caojun@17paipai.cn>
2025-08-12 17:24:50 +0800

    战斗流水字段补全

--- io/kafka/mazebarrieruserkafka/maze_barrier_user_kafka.go
+++ io/kafka/mazebarrieruserkafka/maze_barrier_user_kafka.go
@@ -22,7 +22,7 @@ var d = dispatcher.NewDispatcher[*MazeBarrierUserGameRecord]()
 type MazeBarrierUserGameRecord struct {
 	UserId     uint64 `json:"user_id" gorm:"column:user_id"`         // 用户id
 	Barrier    int32  `json:"barrier" gorm:"column:barrier"`         // 关卡id
-	GameRet    int32  `json:"game_ret" gorm:"-"`                     // 用户闯关结果 1-通关成功 2-死亡失败 3-扫荡
+	GameRet    int32  `json:"game_ret" gorm:"column:game_ret"`       // 用户闯关结果 1-通关成功 2-死亡失败 3-扫荡
 	Awards     string `json:"awards" gorm:"column:awards"`           // 本次获得的奖励
 	GroupID    uint32 `json:"group_id" gorm:"column:group_id"`       // 组id
 	CreateTime int64  `json:"create_time" gorm:"column:create_time"` // 操作时间
