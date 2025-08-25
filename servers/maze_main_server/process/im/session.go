23e38a885f0a8fbf2a3a1007056952f16fbddf3a
chanyanbing <chanyanbing@17paipai.cn>
2025-08-25 17:36:17 +0800

    增加会话列表查询接口

--- servers/maze_main_server/process/im/session.go
+++ servers/maze_main_server/process/im/session.go
@@ -1,1 +1,41 @@
 package im
+
+import (
+	"maze_game_server/app"
+	"maze_game_server/common/errors"
+	"maze_game_server/lib/log"
+	"maze_game_server/lib/nano/session"
+	"maze_game_server/pb/common/MazeIM"
+	"maze_game_server/services/sessionservice"
+
+	"gitlab.ifreetalk.com/maze-plate/freetk/fkcore/fkprometheus"
+	"go.uber.org/zap"
+)
+
+func (im *IM) OnQueryRecentSessions_10652_10653(s *session.Session, req *MazeIM.QueryRecentSessionsRQ) (err error) {
+	defer fkprometheus.InfoPMT("OnQueryMessages")()
+
+	logger := log.Clone("Game", uint64(s.UID()), 0)
+	res := &MazeIM.QueryRecentSessionsRS{}
+	res.Header = req.Header
+	res.ErrInfo = errors.NO_ERROR
+
+	logger.InfoWF("OnQueryMessages start", zap.Any("req", req))
+	defer func() {
+		err = s.Response(res)
+		logger.InfoWF("OnQueryMessages end", zap.Any("res", res))
+	}()
+
+	var (
+		userId = uint64(s.UID())
+	)
+
+	user, err := app.WrapUser(userId, "")
+	if err != nil {
+		return err
+	}
+
+	messages, err := sessionservice.Default.QueryRecentSessions(s.Context(), logger, app.Maze, user)
+	_ = messages
+	return
+}
