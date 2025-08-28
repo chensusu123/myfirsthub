8434e6047f24a7be442d84aeb53ef816ecb1d23a
hujiahong <hujiahong@17paipai.cn>
2025-08-28 20:48:08 +0800

    jenkins自动提交-20250828-20_48_07

--- config/GMazeEnergyAffixLibraryV8Cfg/MazeEnergyAffixLibraryV8.go
+++ config/GMazeEnergyAffixLibraryV8Cfg/MazeEnergyAffixLibraryV8.go
@@ -1,24 +1,27 @@
 package GMazeEnergyAffixLibraryV8Cfg
 
+
 import (
 	"context"
-	"errors"
-	"gitlab.ifreetalk.com/maze-plate/freetk/fkcore/fklog"
-	"gitlab.ifreetalk.com/maze-plate/freetk/fkserver/config_manager"
-	"go.uber.org/zap"
-	"strconv"
-	"strings"
 	"sync"
 	"sync/atomic"
 	"unsafe"
+	"strconv"
+	"errors"
+	"strings"
+	"gitlab.ifreetalk.com/maze-plate/freetk/fkserver/config_manager"
+	"gitlab.ifreetalk.com/maze-plate/freetk/fkcore/fklog"
+	"go.uber.org/zap"
 )
+
 
 // MazeEnergyAffixLibraryV8ConfigRow from maze_energy_affix_library_v8【迷宫-能量词条库权重】.xlsx maze_energy_affix_library_v8
 type MazeEnergyAffixLibraryV8ConfigRow struct {
-	Order                   int32   `json:"order"`                   // 词条库id
-	Certainly_affix_id_list []int32 `json:"certainly_affix_id_list"` // 必出库包括的词条id
-	Affix_id_list           []int32 `json:"affix_id_list"`           // 库包括的词条id
-	Weight                  int32   `json:"weight"`                  // 库随机权重
+    Order       int32  `json:"order"` // 词条库id
+    Certainly_affix_id_list       []int32  `json:"certainly_affix_id_list"` // 必出库包括的词条id
+    Affix_id_list       []int32  `json:"affix_id_list"` // 库包括的词条id
+    Weight_adjust1_add       []int32  `json:"weight_adjust1_add"` // 库词条已选次数影响权重系数（加值\万分比）
+    Weight_adjust2_sub       []int32  `json:"weight_adjust2_sub"` // 库已随次数影响权重系数（减值\万分比）
 }
 
 // MazeEnergyAffixLibraryV8Config from maze_energy_affix_library_v8【迷宫-能量词条库权重】.xlsx maze_energy_affix_library_v8
@@ -54,7 +57,7 @@ func (c *MazeEnergyAffixLibraryV8Config) Get(configId int32) *MazeEnergyAffixLib
 }
 
 // GetAllMazeEnergyAffixLibraryV8Config get all config slice
-func (c *MazeEnergyAffixLibraryV8Config) GetAllMazeEnergyAffixLibraryV8Config() (res []*MazeEnergyAffixLibraryV8ConfigRow) {
+func (c *MazeEnergyAffixLibraryV8Config)  GetAllMazeEnergyAffixLibraryV8Config () (res []*MazeEnergyAffixLibraryV8ConfigRow) {
 	// c.lock.RLock()
 	// defer c.lock.RUnlock()
 	for _, val := range c.ConfigRows {
@@ -64,7 +67,7 @@ func (c *MazeEnergyAffixLibraryV8Config) GetAllMazeEnergyAffixLibraryV8Config() 
 }
 
 // GetAll get all config slice
-func (c *MazeEnergyAffixLibraryV8Config) GetAll() (res []*MazeEnergyAffixLibraryV8ConfigRow) {
+func (c *MazeEnergyAffixLibraryV8Config)  GetAll() (res []*MazeEnergyAffixLibraryV8ConfigRow) {
 	// c.lock.RLock()
 	// defer c.lock.RUnlock()
 	for _, val := range c.ConfigRows {
@@ -73,8 +76,9 @@ func (c *MazeEnergyAffixLibraryV8Config) GetAll() (res []*MazeEnergyAffixLibrary
 	return
 }
 
-// global config pointer
-var gConfigData *MazeEnergyAffixLibraryV8Config
+
+// global config pointer 
+var gConfigData *MazeEnergyAffixLibraryV8Config 
 
 // GetMazeEnergyAffixLibraryV8Config pkg func. get one config by configId
 func GetMazeEnergyAffixLibraryV8Config(configId int32) *MazeEnergyAffixLibraryV8ConfigRow {
@@ -97,8 +101,8 @@ func GetWithCtx(ctx context.Context, configId int32, otps ...config_manager.Quer
 }
 
 // GetAllMazeEnergyAffixLibraryV8Config pkg func. get all config slice
-func GetAllMazeEnergyAffixLibraryV8Config() []*MazeEnergyAffixLibraryV8ConfigRow {
-	return gConfigData.GetAllMazeEnergyAffixLibraryV8Config()
+func GetAllMazeEnergyAffixLibraryV8Config () []*MazeEnergyAffixLibraryV8ConfigRow {
+	return gConfigData.GetAllMazeEnergyAffixLibraryV8Config ()
 }
 
 // GetAll pkg func. get all config slice
@@ -107,17 +111,17 @@ func GetAll() []*MazeEnergyAffixLibraryV8ConfigRow {
 }
 
 // ConfigRows get raw map data
-func ConfigRows() map[int32]*MazeEnergyAffixLibraryV8ConfigRow {
+func ConfigRows() map[int32]*MazeEnergyAffixLibraryV8ConfigRow{
 	return gConfigData.ConfigRows
 }
 
 // GetConfigDesc get config desc for lod,debug etc.
-func GetConfigDesc() string {
+func GetConfigDesc() string{
 	return "MazeEnergyAffixLibraryV8ConfigRow from maze_energy_affix_library_v8【迷宫-能量词条库权重】.xlsx maze_energy_affix_library_v8"
 }
 
 // GetRawValue get raw data
-func GetRawValue() *MazeEnergyAffixLibraryV8Config {
+func GetRawValue() *MazeEnergyAffixLibraryV8Config{
 	return gConfigData
 }
 
@@ -127,10 +131,10 @@ func SheetName() string {
 }
 
 func init() {
-	// reg config auto load
-	config_manager.RegAutoConfig("maze_energy_affix_library_v8.json",
+	// reg config auto load 
+	config_manager.RegAutoConfig("maze_energy_affix_library_v8.json", 
 		"maze_energy_affix_library_v8【迷宫-能量词条库权重】.xlsx", "maze_energy_affix_library_v8",
-		&gMazeEnergyAffixLibraryV8Parser{}, &gMazeEnergyAffixLibraryV8Loader{})
+	 	&gMazeEnergyAffixLibraryV8Parser{}, &gMazeEnergyAffixLibraryV8Loader{})
 }
 
 // data update call back
@@ -158,35 +162,32 @@ var cfgCheckCallBack sync.Map //map[string]func(*MazeEnergyAffixLibraryV8Config)
 // doConfigCheckCallback do config check callback
 func doConfigCheckCallback(c *MazeEnergyAffixLibraryV8Config) (err error) {
 	cfgCheckCallBack.Range(func(key, value interface{}) bool {
-		err := value.(func(*MazeEnergyAffixLibraryV8Config) error)(c)
+		err := value.(func(*MazeEnergyAffixLibraryV8Config)error)(c)
 		return err == nil
 	})
-	return
+	return 
 }
 
 // RegisterLoadedCallBack reg config update func.
-func RegisterConfigCheck(key string, f func(*MazeEnergyAffixLibraryV8Config) error) {
+func RegisterConfigCheck(key string, f func(*MazeEnergyAffixLibraryV8Config)error) {
 	cfgCheckCallBack.Store(key, f)
 }
 
 // implete ConfigLoader interface
 type gMazeEnergyAffixLibraryV8Loader struct {
 }
-
 // NewContainer new data container pointer
-func (*gMazeEnergyAffixLibraryV8Loader) NewContainer() interface{} {
+func (*gMazeEnergyAffixLibraryV8Loader) NewContainer() interface{}{
 	return newConfig()
 }
-
 // Check check new config data ptr
-func (*gMazeEnergyAffixLibraryV8Loader) Check(newPtr interface{}) error {
+func (*gMazeEnergyAffixLibraryV8Loader) Check(newPtr interface{})error{
 	// set global ptr
 	cfgData := newPtr.(*MazeEnergyAffixLibraryV8Config)
 	return doConfigCheckCallback(cfgData)
 }
-
 // Swap swap global config data ptr
-func (*gMazeEnergyAffixLibraryV8Loader) Swap(newPtr interface{}) {
+func (*gMazeEnergyAffixLibraryV8Loader) Swap(newPtr interface{}){
 	// convert pointer
 	cache := newPtr.(*MazeEnergyAffixLibraryV8Config)
 	// update second edit
@@ -194,48 +195,45 @@ func (*gMazeEnergyAffixLibraryV8Loader) Swap(newPtr interface{}) {
 	// set global ptr
 	atomic.StorePointer((*unsafe.Pointer)(unsafe.Pointer(&gConfigData)), unsafe.Pointer(cache))
 }
-
 // Add append config item to map,save result
-func (*gMazeEnergyAffixLibraryV8Loader) Add(logger fklog.FKLogI, container interface{}, ri interface{}) (err error) {
-	row, ok := ri.(*MazeEnergyAffixLibraryV8ConfigRow)
+func (*gMazeEnergyAffixLibraryV8Loader) Add(logger fklog.FKLogI,container interface{}, ri interface{})(err error) {
+	row,ok := ri.(*MazeEnergyAffixLibraryV8ConfigRow)
 	if !ok {
 		err = errors.New("invalid type. not *MazeEnergyAffixLibraryV8ConfigRow")
 		logger.ErrorWF("invalid type. not *MazeEnergyAffixLibraryV8ConfigRow", zap.String("xlsx", "maze_energy_affix_library_v8【迷宫-能量词条库权重】.xlsx"),
 			zap.String("sheet", "maze_energy_affix_library_v8"))
-		return
+		return 
 	}
-	config, ok := container.(*MazeEnergyAffixLibraryV8Config)
+	config,ok := container.(*MazeEnergyAffixLibraryV8Config)
 	if !ok {
 		err = errors.New("invalid type. not *MazeEnergyAffixLibraryV8Config")
 		logger.ErrorWF("invalid type. not *MazeEnergyAffixLibraryV8Config", zap.String("xlsx", "maze_energy_affix_library_v8【迷宫-能量词条库权重】.xlsx"),
 			zap.String("sheet", "maze_energy_affix_library_v8"))
-		return
+		return 
 	}
 	config.ConfigRows[row.Order] = row
 	return
 }
-
 // GetValue get real map value for json parse
-func (*gMazeEnergyAffixLibraryV8Loader) GetValue(logger fklog.FKLogI, container interface{}) (real interface{}, err error) {
-	config, ok := container.(*MazeEnergyAffixLibraryV8Config)
+func (*gMazeEnergyAffixLibraryV8Loader) GetValue(logger fklog.FKLogI,container interface{})(real interface{},err error) {
+	config,ok := container.(*MazeEnergyAffixLibraryV8Config)
 	if !ok {
 		err = errors.New("invalid type. not *MazeEnergyAffixLibraryV8Config")
 		logger.ErrorWF("invalid type. not *MazeEnergyAffixLibraryV8Config", zap.String("xlsx", "maze_energy_affix_library_v8【迷宫-能量词条库权重】.xlsx"),
 			zap.String("sheet", "maze_energy_affix_library_v8"))
-		return
+		return 
 	}
 	real = &config.ConfigRows
 	return
 }
-
 // Range range all data for json append data parse.
-func (*gMazeEnergyAffixLibraryV8Loader) Range(logger fklog.FKLogI, container interface{}, rf func(row interface{}) error) (err error) {
-	config, ok := container.(*MazeEnergyAffixLibraryV8Config)
+func (*gMazeEnergyAffixLibraryV8Loader) Range(logger fklog.FKLogI, container interface{}, rf func(row interface{}) error) (err error){
+	config,ok := container.(*MazeEnergyAffixLibraryV8Config)
 	if !ok {
 		err = errors.New("invalid type. not *MazeEnergyAffixLibraryV8Config")
 		logger.ErrorWF("invalid type. not *MazeEnergyAffixLibraryV8Config", zap.String("xlsx", "maze_energy_affix_library_v8【迷宫-能量词条库权重】.xlsx"),
 			zap.String("sheet", "maze_energy_affix_library_v8"))
-		return
+		return 
 	}
 	for _, row := range config.ConfigRows {
 		err = rf(row)
@@ -246,10 +244,10 @@ func (*gMazeEnergyAffixLibraryV8Loader) Range(logger fklog.FKLogI, container int
 	return
 }
 
+
 // implete ConfigParser interface
 type gMazeEnergyAffixLibraryV8Parser struct {
 }
-
 // New new config row data
 func (*gMazeEnergyAffixLibraryV8Parser) New() interface{} {
 	return &MazeEnergyAffixLibraryV8ConfigRow{}
@@ -259,55 +257,54 @@ func (*gMazeEnergyAffixLibraryV8Parser) New() interface{} {
 func (*gMazeEnergyAffixLibraryV8Parser) Fields() []string {
 	return gMazeEnergyAffixLibraryV8Fields
 }
-
 // Parse parse raw data to row data
-func (*gMazeEnergyAffixLibraryV8Parser) Parse(logger fklog.FKLogI, data []string, row interface{}) (err error) {
+func (*gMazeEnergyAffixLibraryV8Parser) Parse(logger fklog.FKLogI,data []string, row interface{}) (err error) {
 	// convert row type
-	config, ok := row.(*MazeEnergyAffixLibraryV8ConfigRow)
+	config,ok := row.(*MazeEnergyAffixLibraryV8ConfigRow)
 	if !ok {
 		err = errors.New("invalid type. not *MazeEnergyAffixLibraryV8ConfigRow")
 		logger.ErrorWF("invalid type. not *MazeEnergyAffixLibraryV8ConfigRow", zap.String("xlsx", "maze_energy_affix_library_v8【迷宫-能量词条库权重】.xlsx"),
 			zap.String("sheet", "maze_energy_affix_library_v8"))
-		return
+		return 
 	}
 	// compare length
 	if len(data) != len(gMazeEnergyAffixLibraryV8Fields) {
 		err = errors.New("fields count not match.")
-		logger.ErrorWF("invalid type. not *map[int32]*MazeEnergyAffixLibraryV8ConfigRow",
+		logger.ErrorWF("invalid type. not *map[int32]*MazeEnergyAffixLibraryV8ConfigRow", 
 			zap.String("xlsx", "maze_energy_affix_library_v8【迷宫-能量词条库权重】.xlsx"),
-			zap.String("sheet", "maze_energy_affix_library_v8"), zap.Int("need_count", len(gMazeEnergyAffixLibraryV8Fields)),
-			zap.Int("had_count", len(data)))
+			zap.String("sheet", "maze_energy_affix_library_v8"), zap.Int("need_count",len(gMazeEnergyAffixLibraryV8Fields)), 
+			zap.Int("had_count",len(data)))
 		return
 	}
 
 	var tmp int64
 
-	// parse column 0 order : 词条库id
+	// parse column 0 order : 词条库id 
 	if data[0] != "" {
-		tmp, err = strconv.ParseInt(data[0], 10, 64)
+		tmp,err = strconv.ParseInt(data[0],10,64)
 		if err != nil {
 			err = errors.New("parse field order 词条库id to int32 failed")
-			logger.ErrorWF("parse field order 词条库id to int32 failed.",
-				zap.String("xlsx", "maze_energy_affix_library_v8【迷宫-能量词条库权重】.xlsx"), zap.String("sheet", "maze_energy_affix_library_v8"),
-				zap.String("parse_data", data[0]),
+			logger.ErrorWF("parse field order 词条库id to int32 failed.", 
+				zap.String("xlsx", "maze_energy_affix_library_v8【迷宫-能量词条库权重】.xlsx"), zap.String("sheet", "maze_energy_affix_library_v8"), 
+				zap.String("parse_data",data[0]), 
 				zap.Error(err))
 			return
 		}
 		config.Order = int32(tmp)
 	}
 
-	// parse column 1 certainly_affix_id_list : 必出库包括的词条id
+	// parse column 1 certainly_affix_id_list : 必出库包括的词条id 
 	if data[1] != "" {
-
-		vals := strings.Split(data[1], ",")
-		for k, v := range vals {
-			tmp, err = strconv.ParseInt(v, 10, 64)
+    
+		vals := strings.Split(data[1],",")
+		for k,v := range vals {
+			tmp,err = strconv.ParseInt(v,10,64)
 			if err != nil {
 				err = errors.New("parse array field certainly_affix_id_list 必出库包括的词条id to []int32 failed")
-				logger.ErrorWF("parse array field certainly_affix_id_list 必出库包括的词条id to []int32 failed.",
-					zap.String("xlsx", "maze_energy_affix_library_v8【迷宫-能量词条库权重】.xlsx"), zap.String("sheet", "maze_energy_affix_library_v8"),
-					// zap.String("field_data",data[1]),
-					zap.String("parse_data", v), zap.Int("index", k),
+				logger.ErrorWF("parse array field certainly_affix_id_list 必出库包括的词条id to []int32 failed.", 
+					zap.String("xlsx", "maze_energy_affix_library_v8【迷宫-能量词条库权重】.xlsx"), zap.String("sheet", "maze_energy_affix_library_v8"), 
+					// zap.String("field_data",data[1]), 
+					zap.String("parse_data", v),zap.Int("index", k),
 					zap.Error(err))
 				return
 			}
@@ -315,18 +312,18 @@ func (*gMazeEnergyAffixLibraryV8Parser) Parse(logger fklog.FKLogI, data []string
 		}
 	}
 
-	// parse column 2 affix_id_list : 库包括的词条id
+	// parse column 2 affix_id_list : 库包括的词条id 
 	if data[2] != "" {
-
-		vals := strings.Split(data[2], ",")
-		for k, v := range vals {
-			tmp, err = strconv.ParseInt(v, 10, 64)
+    
+		vals := strings.Split(data[2],",")
+		for k,v := range vals {
+			tmp,err = strconv.ParseInt(v,10,64)
 			if err != nil {
 				err = errors.New("parse array field affix_id_list 库包括的词条id to []int32 failed")
-				logger.ErrorWF("parse array field affix_id_list 库包括的词条id to []int32 failed.",
-					zap.String("xlsx", "maze_energy_affix_library_v8【迷宫-能量词条库权重】.xlsx"), zap.String("sheet", "maze_energy_affix_library_v8"),
-					// zap.String("field_data",data[2]),
-					zap.String("parse_data", v), zap.Int("index", k),
+				logger.ErrorWF("parse array field affix_id_list 库包括的词条id to []int32 failed.", 
+					zap.String("xlsx", "maze_energy_affix_library_v8【迷宫-能量词条库权重】.xlsx"), zap.String("sheet", "maze_energy_affix_library_v8"), 
+					// zap.String("field_data",data[2]), 
+					zap.String("parse_data", v),zap.Int("index", k),
 					zap.Error(err))
 				return
 			}
@@ -334,35 +331,60 @@ func (*gMazeEnergyAffixLibraryV8Parser) Parse(logger fklog.FKLogI, data []string
 		}
 	}
 
-	// parse column 3 weight : 库随机权重
+	// parse column 3 weight_adjust1_add : 库词条已选次数影响权重系数（加值\万分比） 
 	if data[3] != "" {
-		tmp, err = strconv.ParseInt(data[3], 10, 64)
-		if err != nil {
-			err = errors.New("parse field weight 库随机权重 to int32 failed")
-			logger.ErrorWF("parse field weight 库随机权重 to int32 failed.",
-				zap.String("xlsx", "maze_energy_affix_library_v8【迷宫-能量词条库权重】.xlsx"), zap.String("sheet", "maze_energy_affix_library_v8"),
-				zap.String("parse_data", data[3]),
-				zap.Error(err))
-			return
+    
+		vals := strings.Split(data[3],",")
+		for k,v := range vals {
+			tmp,err = strconv.ParseInt(v,10,64)
+			if err != nil {
+				err = errors.New("parse array field weight_adjust1_add 库词条已选次数影响权重系数（加值\万分比） to []int32 failed")
+				logger.ErrorWF("parse array field weight_adjust1_add 库词条已选次数影响权重系数（加值\万分比） to []int32 failed.", 
+					zap.String("xlsx", "maze_energy_affix_library_v8【迷宫-能量词条库权重】.xlsx"), zap.String("sheet", "maze_energy_affix_library_v8"), 
+					// zap.String("field_data",data[3]), 
+					zap.String("parse_data", v),zap.Int("index", k),
+					zap.Error(err))
+				return
+			}
+			config.Weight_adjust1_add = append(config.Weight_adjust1_add, int32(tmp))
 		}
-		config.Weight = int32(tmp)
+	}
+
+	// parse column 4 weight_adjust2_sub : 库已随次数影响权重系数（减值\万分比） 
+	if data[4] != "" {
+    
+		vals := strings.Split(data[4],",")
+		for k,v := range vals {
+			tmp,err = strconv.ParseInt(v,10,64)
+			if err != nil {
+				err = errors.New("parse array field weight_adjust2_sub 库已随次数影响权重系数（减值\万分比） to []int32 failed")
+				logger.ErrorWF("parse array field weight_adjust2_sub 库已随次数影响权重系数（减值\万分比） to []int32 failed.", 
+					zap.String("xlsx", "maze_energy_affix_library_v8【迷宫-能量词条库权重】.xlsx"), zap.String("sheet", "maze_energy_affix_library_v8"), 
+					// zap.String("field_data",data[4]), 
+					zap.String("parse_data", v),zap.Int("index", k),
+					zap.Error(err))
+				return
+			}
+			config.Weight_adjust2_sub = append(config.Weight_adjust2_sub, int32(tmp))
+		}
 	}
 	return
 }
 
 var gMazeEnergyAffixLibraryV8Fields = []string{
-	"order",
-	"certainly_affix_id_list",
-	"affix_id_list",
-	"weight",
+    "order",
+    "certainly_affix_id_list",
+    "affix_id_list",
+    "weight_adjust1_add",
+    "weight_adjust2_sub",
 }
 
 // LoadDataManual load data for test
-func LoadDataManual(logger fklog.FKLogI, load func(file, sheet string, fields []string) ([][]string, error)) (err error) {
+func LoadDataManual(logger fklog.FKLogI, load func(file, sheet string, fields []string) ([][]string,error)) (err error) {
 	parser := &gMazeEnergyAffixLibraryV8Parser{}
 	loader := &gMazeEnergyAffixLibraryV8Loader{}
 	var data [][]string
-	data, err = load("maze_energy_affix_library_v8【迷宫-能量词条库权重】.xlsx", "maze_energy_affix_library_v8", gMazeEnergyAffixLibraryV8Fields)
+	data,err = load("maze_energy_affix_library_v8【迷宫-能量词条库权重】.xlsx", "maze_energy_affix_library_v8", gMazeEnergyAffixLibraryV8Fields)
 	if err != nil {
 		logger.ErrorWF("load maze_energy_affix_library_v8【迷宫-能量词条库权重】.xlsx maze_energy_affix_library_v8 data failed.", zap.Error(err))
 		return
