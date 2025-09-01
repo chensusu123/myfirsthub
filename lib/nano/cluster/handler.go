af72c173b40ec943c21e7fb9dfaaadf15ffbca66
jiangema <majiange@126.com>
2025-09-01 23:07:45 +0800

    fix: 移除消息处理中的超时控制逻辑

--- lib/nano/cluster/handler.go
+++ lib/nano/cluster/handler.go
@@ -549,13 +549,13 @@ func (h *LocalHandler) remoteProcess(ctx context.Context, session *session.Sessi
 }
 
 func (h *LocalHandler) processMessage(ctx context.Context, agent *agent, msg *message.Message) {
-	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	// ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer func() {
 		fkalert.RecoverAlertException()
-		cancel()
-		if ctx.Err() != nil {
-			fklog.ContextAppLogger(ctx).ErrorWF("process message timeout", zap.Error(ctx.Err()))
-		}
+		// cancel()
+		// if ctx.Err() != nil {
+		// 	fklog.ContextAppLogger(ctx).ErrorWF("process message timeout", zap.Error(ctx.Err()))
+		// }
 	}()
 
 	var lastMid uint64
