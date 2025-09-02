aede10b628d9ec721d9916e39216bcf0d6b45dc0
jiangema <majiange@126.com>
2025-09-02 19:46:00 +0800

    fix: 修改debug_ty.sh中的HOSTNAME和APPNAME配置

--- servers/maze_main_server/debug_ty.sh
+++ servers/maze_main_server/debug_ty.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mode=dev POLARIS_CONF_ADRR=10.101.110.52:8093,10.101.110.53:8093,10.101.110.54:8093 POLARIS_SERVER_ADRR=10.101.110.52:8091,10.101.110.53:8091,10.101.110.54:8091 HOSTNAME=maze-main-server-c0-g4-0 S_GROUP_ID=4 APPNAME=maze-main-server SERVER_ID=4 NAMESPACE=adl-ty POD_LOCAL_IP=127.0.0.1 go run main.go conf.d/config.ini logs/maze-main-server maze-main-server
+mode=dev POLARIS_CONF_ADRR=10.101.110.52:8093,10.101.110.53:8093,10.101.110.54:8093 POLARIS_SERVER_ADRR=10.101.110.52:8091,10.101.110.53:8091,10.101.110.54:8091 HOSTNAME=test-main-server-c0-g4-0 S_GROUP_ID=4 APPNAME=test-main-server SERVER_ID=4 NAMESPACE=adl-ty POD_LOCAL_IP=127.0.0.1 go run main.go conf.d/config.ini logs/test-main-server test-main-server
