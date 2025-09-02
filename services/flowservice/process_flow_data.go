bcca7d5b20bc5cd15140fd646fe7fcb26c73674a
曹峻 <caojun@17paipai.cn>
2025-09-02 10:54:45 +0800

    fix(flow): 修复流水协程退出问题 增加重启协程
    修复流水协程退出问题 增加重启协程

--- services/flowservice/process_flow_data.go
+++ services/flowservice/process_flow_data.go
@@ -1,19 +1,39 @@
 package flowservice
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"maze_game_server/io/kafka"
+	flowmodel "maze_game_server/model/flowmodel/flow_model"
 	"time"
 
+	"github.com/bytedance/gopkg/util/logger"
 	"gitlab.ifreetalk.com/maze-plate/freetk/fkcore/fklog"
 	"go.uber.org/zap"
 )
 
 func (s *service) ProcessFlowData() {
+	defer func() {
+		if err := recover(); err != nil {
+			logger.CtxErrorf(context.TODO(), "flowservice ProcessFlowData Restart")
+			// 重启该携程 防止panic退出
+			go s.ProcessFlowData()
+		}
+	}()
+
 	for {
 		select {
 		case data := <-s.ch:
+			// 检测容量告警
+			if len(s.ch) >= flowmodel.ERRORFLOWCHANSIZE {
+				logger.CtxErrorf(context.TODO(), "flowservice ProcessFlowData Size Greater than ERRORFLOWCHANSIZE",
+					zap.Any("ERRORFLOWCHANSIZE", flowmodel.ERRORFLOWCHANSIZE))
+			} else if len(s.ch) >= flowmodel.WARNFLOWCHANSIZE {
+				logger.CtxWarnf(context.TODO(), "flowservice ProcessFlowData Size Greater than WARNFLOWCHANSIZE",
+					zap.Any("ERRORFLOWCHANSIZE", flowmodel.WARNFLOWCHANSIZE))
+			}
+
 			// 打到kafka 中
 			ctx, record := data.Ctx, data.Data
 			logger := fklog.ContextAppLogger(ctx)
@@ -24,15 +44,15 @@ func (s *service) ProcessFlowData() {
 					zap.Any("record", record),
 					zap.Error(err),
 				)
-				return
+				continue
 			}
+
 			err = kafka.GflowKafka.SendMsg(ctx, fmt.Sprintf("%v", time.Now().UnixNano()), jsonData)
 			if err != nil {
 				logger.CtxError(ctx, "flowservice ProcessFlowData SendMsg Fail",
 					zap.Any("record", record),
 					zap.Error(err),
 				)
-				return
 			}
 		}
 	}
