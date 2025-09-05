bbac155ba259569208f4ebcbfbb795836936b8d0
jiangema <majiange@126.com>
2025-09-05 10:57:35 +0800

    fix(cluster): 修复safeSend函数并优化agent响应处理
    重构safeSend函数以返回操作状态并添加指标上报
    在pendingMessage结构中添加isResponse字段以区分响应类型
    优化_responseMid函数处理逻辑并添加header标记
    处理safeSend失败情况并记录错误信息

--- lib/nano/cluster/agent.go
+++ lib/nano/cluster/agent.go
@@ -89,13 +89,14 @@ type (
 	}
 
 	pendingMessage struct {
-		uid     int64
-		ctx     context.Context
-		typ     message.Type // message type
-		route   string       // message route(push)
-		mid     uint64       // response message id(response)
-		payload interface{}  // payload
-		isBytes bool
+		uid        int64
+		ctx        context.Context
+		typ        message.Type // message type
+		route      string       // message route(push)
+		mid        uint64       // response message id(response)
+		payload    interface{}  // payload
+		isBytes    bool
+		isResponse bool
 	}
 )
 
@@ -214,15 +215,25 @@ func (a *agent) RPC(ctx context.Context, route string, v interface{}) error {
 // Response, implementation for session.NetworkEntity interface
 // Response message to session
 func (a *agent) Response(ctx context.Context, v interface{}) error {
-	return a.ResponseMid(ctx, a.lastMid, v)
+	return a._responseMid(ctx, a.lastMid, v, true)
+}
+
+func (a *agent) ResponseMid(ctx context.Context, mid uint64, v interface{}) error {
+	return a._responseMid(ctx, mid, v, false)
 }
 
 // ResponseMid, implementation for session.NetworkEntity interface
 // Response message to session
-func (a *agent) ResponseMid(ctx context.Context, mid uint64, v interface{}) error {
+func (a *agent) _responseMid(ctx context.Context, mid uint64, v interface{}, isResponse bool) error {
 	userID := a.session.UID()
 	agentSession := a.session.ID()
-	ctx, span := agentSendSpan(ctx, "response", mid, userID, agentSession)
+	var sendTypeName string
+	if isResponse {
+		sendTypeName = "response"
+	} else {
+		sendTypeName = "notify"
+	}
+	ctx, span := agentSendSpan(ctx, sendTypeName, mid, userID, agentSession)
 	defer span.End()
 	if a.status() == statusClosed {
 		span.RecordError(ErrBrokenPipe)
@@ -253,7 +264,7 @@ func (a *agent) ResponseMid(ctx context.Context, mid uint64, v interface{}) erro
 		}
 	}
 
-	err := a.send(pendingMessage{ctx: ctx, typ: message.Response, mid: mid, payload: v, uid: userID})
+	err := a.send(pendingMessage{ctx: ctx, typ: message.Response, mid: mid, payload: v, uid: userID, isResponse: isResponse})
 	if err != nil {
 		span.RecordError(err)
 		span.SetStatus(codes.Error, err.Error())
@@ -599,7 +610,9 @@ func processPendingMessage(a *agent, data pendingMessage, chWrite chan WriteItem
 		xxx := &raw_pkg.StruSvrEsRawBaseHead{
 			Header: make(map[string]string),
 		}
-
+		if data.isResponse {
+			xxx.Header["X-isResponse"] = "1"
+		}
 		carrier := otel.GetTextMapPropagator()
 		carrier.Inject(ctx, packCodec.NewBaseHeader(xxx))
 		hhh := svrheader.SvrHeader{
@@ -607,15 +620,20 @@ func processPendingMessage(a *agent, data pendingMessage, chWrite chan WriteItem
 			Body:   p,
 		}
 
-		yyy, err := hhh.Encode()
-		if err != nil {
-			span.RecordError(err)
+		span.AddEvent("svrheader.Encode")
+		yyy, errEncode := hhh.Encode()
+		if errEncode != nil {
+			span.RecordError(errEncode)
 			span.SetStatus(codes.Error, "svrheader.Encode failed.")
-			return err
+			return errEncode
 		}
 		p = yyy
 	}
-	safeSend(chWrite, WriteItem{ctx: ctx, data: p})
+	ok := safeSend(chWrite, WriteItem{ctx: ctx, data: p})
+	if !ok {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, "safeSend chWrite failed.")
+	}
 	return nil
 }
 
