fd1789502efbb4366bc4bf997e8b7011cc6d19ac
曹峻 <caojun@17paipai.cn>
2025-09-13 15:58:27 +0800

    feat: 拒绝好友请求

--- servers/maze_main_server/process/friend/handle_reply_friend_apply.go
+++ servers/maze_main_server/process/friend/handle_reply_friend_apply.go
@@ -8,7 +8,6 @@ import (
 	"maze_game_server/services/friendservice"
 	"maze_game_server/services/userprofileservice"
 	"maze_game_server/usecase/online"
-	"time"
 
 	"gitlab.ifreetalk.com/maze-plate/freetk/fkcore/fklog"
 	"go.uber.org/zap"
@@ -48,16 +47,6 @@ func (f *FriendComponent) OnReplyFriendApply_10697_10698(s *session.Session, req
 		}
 	}
 
-	nowUserProfile, err := userprofileservice.GlobalUserProfileService.GetUserProfile(ctx, userId)
-	if err != nil {
-		logger.CtxError(ctx, "OnReplyFriendApply GetUserProfile Fail",
-			zap.Any("userID", userId),
-			zap.Error(err),
-		)
-		res.ErrInfo = errors.COMMON_ERROR_TIPS.ToInfo()
-		return err
-	}
-
 	applyID := &Friend.FriendApplyID{}
 	// 推请求处理包
 	for _, handlerUser := range handlerUserList {
@@ -81,12 +70,6 @@ func (f *FriendComponent) OnReplyFriendApply_10697_10698(s *session.Session, req
 			From:        proto.Int32(handlerUser.From),
 		}
 
-		if req.GetReplyResult() == int32(Friend.REPLY_FRIEND_APPLY_RESULT_AGREE) {
-
-		} else {
-
-		}
-
 		applyID.DelReceiveInfo = append(applyID.DelReceiveInfo, &Friend.ReceiveInfo{
 			UserInfo: &Friend.User{
 				UserId:     proto.Int64(int64(handlerUser.FromUserId)),
@@ -100,24 +83,6 @@ func (f *FriendComponent) OnReplyFriendApply_10697_10698(s *session.Session, req
 
 		// 推好友请求处理包
 		online.ClusterPush(ctx, handlerUser.FromUserId, 10699, pushMsg)
-
-		nowTime := time.Now()
-		// 推给对方好友请求变化包
-		toApplyID := &Friend.FriendApplyID{
-			DelReceiveInfo: []*Friend.ReceiveInfo{
-				{
-					UserInfo: &Friend.User{
-						UserId:     proto.Int64(s.UID()),
-						UserName:   proto.String(nowUserProfile.NickName),
-						UserGender: proto.Int32(nowUserProfile.Sex),
-						AvaterUrl:  proto.String(nowUserProfile.Avatar),
-					},
-					ReceiveTime: proto.Int64(nowTime.UnixMilli()),
-					ExpireTime:  proto.Int64(nowTime.UnixMilli() + int64(friendmodel.ExpireTime)),
-				},
-			},
-		}
-		online.ClusterPush(ctx, handlerUser.FromUserId, 10696, toApplyID)
 	}
 
 	// 推好友请求变化包
